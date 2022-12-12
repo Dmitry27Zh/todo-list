93d4b88010588db46f1cae429b2e56e1ab9dd5da
Dmitry <dmitry27zh@gmail.com>
2022-12-12 17:19:41 +0500

    Add task status change

--- js/modules/todo.js
+++ js/modules/todo.js
@@ -1,25 +1,50 @@
 const initTodo = () => {
-  const form = document.querySelector('.todo__form')
   const data = { tasks: [] }
   const tasks = data.tasks
+  const taskElements = new Map()
+  const form = document.querySelector('.todo__form')
+  const taskContainer = form.querySelector('.todo__list')
 
   if (!form) {
     return
   }
 
-  initNewTaskAddition(tasks, form)
+  initTaskList(taskElements, taskContainer)
+  initNewTaskAddition(tasks, form, taskElements, taskContainer)
 }
 
-const initNewTaskAddition = (tasks, form) => {
+const initTaskList = (taskElements, taskContainer) => {
+  taskContainer.addEventListener('click', (e) => {
+    if (!e.target.matches('.todo__btn')) {
+      return
+    }
+
+    e.preventDefault()
+    const taskElement = e.target.closest('.todo__task')
+    const task = taskElements.get(taskElement)
+
+    if (task) {
+      task.done = !task.done
+    }
+
+    if (e.target.matches('.todo__task-status')) {
+      const statusCheckbox = e.target.querySelector('.todo__task-status-input')
+      statusCheckbox.checked = task.done
+    }
+  })
+}
+
+const initNewTaskAddition = (tasks, form, taskElements, taskContainer) => {
   const newTaskInput = form.querySelector('.todo__new-input')
-  const taskContainer = form.querySelector('.todo__list')
 
   form.addEventListener('submit', (e) => {
     e.preventDefault()
     const title = newTaskInput.value.trim()
 
     if (title) {
-      addNewTask(tasks, taskContainer, title)
+      addNewTask(tasks, taskContainer, title, taskElements)
+      newTaskInput.value = ''
+      newTaskInput.focus()
     } else {
       showNewTaskErrorMessage(newTaskInput)
     }
@@ -29,13 +54,14 @@ const initNewTaskAddition = (tasks, form) => {
 const createTaskMarkup = (task) => {
   const { title, done } = task
   const checked = done ? 'checked' : ''
+  const name = `status of «${title}»`
 
   return `
     <li class="todo__task">
       <h3 class="todo__task-title">${title}</h3>
       <label class="todo__btn todo__task-status">
         <span class="visually-hidden">Mark status</span>
-        <input class="todo__task-input visually-hidden" type="checkbox" name="New Task status" ${checked}/>
+        <input class="todo__task-status-input visually-hidden" type="checkbox" name="${name}" ${checked}/>
         <span class="todo__task-icon todo__task-done"><i class="fa-solid fa-check"></i></span>
         <span class="todo__task-icon todo__task-undone">–</span>
       </label>
@@ -50,7 +76,6 @@ const createTaskMarkup = (task) => {
 const showNewTaskErrorMessage = (newTaskInput) => {
   const defaultPlaceholder = newTaskInput.placeholder
   newTaskInput.value = ''
-  newTaskInput.blur()
   newTaskInput.placeholder = 'Error! Non-valid task name'
   newTaskInput.setAttribute('readonly', '')
 
@@ -61,11 +86,12 @@ const showNewTaskErrorMessage = (newTaskInput) => {
   }, 2000)
 }
 
-const addNewTask = (tasks, container, title) => {
+const addNewTask = (tasks, container, title, taskElements) => {
   const task = { title, done: false }
   tasks.push(task)
   const taskMarkup = createTaskMarkup(task)
   container.insertAdjacentHTML('beforeend', taskMarkup)
+  taskElements.set(container.lastElementChild, task)
 }
 
 initTodo()
