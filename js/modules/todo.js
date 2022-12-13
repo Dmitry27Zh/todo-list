1dbc73d974b35ffec7c669bb1590976080225fba
Dmitry <dmitry27zh@gmail.com>
2022-12-13 18:04:49 +0500

    Store data

--- js/modules/todo.js
+++ js/modules/todo.js
@@ -1,19 +1,27 @@
+import { copyObjectWithJSON, replaceAllObjectProperties } from '../utils/object.js'
+
 const initTodo = () => {
   const data = { tasks: [] }
   const tasks = data.tasks
   const taskElements = new Map()
   const form = document.querySelector('.todo__form')
   const taskContainer = form.querySelector('.todo__list')
+  const newTaskInput = form.querySelector('.todo__new-input')
 
   if (!form) {
     return
   }
 
-  initTaskList(tasks, taskElements, taskContainer)
-  initNewTaskAddition(tasks, taskElements, taskContainer, form)
+  initTaskList(tasks, taskElements, taskContainer, form, data, newTaskInput)
+  initNewTaskAddition(tasks, taskElements, taskContainer, form, data, newTaskInput)
 }
 
-const initTaskList = (tasks, taskElements, taskContainer) => {
+const ErrorMessage = {
+  NEW_TASK: 'Error! Non-valid task name',
+  DATA_SAVE: 'Data save is now unavailable!',
+}
+
+const initTaskList = (tasks, taskElements, taskContainer, form, data, newTaskInput) => {
   taskContainer.addEventListener('click', (e) => {
     if (!e.target.matches('.todo__btn')) {
       return
@@ -24,25 +32,37 @@ const initTaskList = (tasks, taskElements, taskContainer) => {
     const task = taskElements.get(taskElement)
 
     if (e.target.matches('.todo__task-status')) {
-      changeTaskStatus(task, e.target)
+      changeTaskStatus(task, e.target, data, newTaskInput, form)
     }
 
     if (e.target.matches('.todo__task-delete')) {
-      deleteTask(task, tasks, taskElement)
+      deleteTask(task, tasks, taskElement, data, newTaskInput, form)
     }
   })
 }
 
-const changeTaskStatus = (task, button) => {
-  task.done = !task.done
-  const statusCheckbox = button.querySelector('.todo__task-status-input')
-  statusCheckbox.checked = task.done
+const changeTaskStatus = (task, button, data, newTaskInput, form) => {
+  const done = !task.done
+
+  changeData(data, () => (task.done = done), newTaskInput, form).then(() => {
+    const statusCheckbox = button.querySelector('.todo__task-status-input')
+    statusCheckbox.checked = done
+  })
 }
 
-const deleteTask = (task, tasks, taskElement) => {
-  const index = tasks.indexOf(task)
-  tasks.splice(index, 1)
-  taskElement.dataset.action = 'delete'
+const deleteTask = (task, tasks, taskElement, data, newTaskInput, form) => {
+  changeData(
+    data,
+    () => {
+      const index = tasks.indexOf(task)
+      tasks.splice(index, 1)
+    },
+    newTaskInput,
+    form
+  ).then(() => {
+    taskElement.dataset.action = 'delete'
+    taskElement.addEventListener('transitionend', onTransitionend)
+  })
 
   const onTransitionend = (e) => {
     if (e.target === taskElement) {
@@ -50,23 +70,19 @@ const deleteTask = (task, tasks, taskElement) => {
       taskElement.removeEventListener('transitionend', onTransitionend)
     }
   }
-
-  taskElement.addEventListener('transitionend', onTransitionend)
 }
 
-const initNewTaskAddition = (tasks, taskElements, taskContainer, form) => {
-  const newTaskInput = form.querySelector('.todo__new-input')
-
+const initNewTaskAddition = (tasks, taskElements, taskContainer, form, data, newTaskInput) => {
   form.addEventListener('submit', (e) => {
     e.preventDefault()
     const title = newTaskInput.value.trim()
 
     if (title) {
-      addNewTask(tasks, taskContainer, title, taskElements)
+      addNewTask(tasks, taskContainer, title, taskElements, data, newTaskInput, form)
       newTaskInput.value = ''
       newTaskInput.focus()
     } else {
-      showNewTaskErrorMessage(newTaskInput)
+      showErrorMessage(newTaskInput, ErrorMessage.NEW_TASK, form)
     }
   })
 }
@@ -93,25 +109,67 @@ const createTaskMarkup = (task) => {
   `
 }
 
-const showNewTaskErrorMessage = (newTaskInput) => {
+const showErrorMessage = (newTaskInput, errorMessage, form) => {
   const defaultPlaceholder = newTaskInput.placeholder
   newTaskInput.value = ''
-  newTaskInput.placeholder = 'Error! Non-valid task name'
+  newTaskInput.placeholder = errorMessage
   newTaskInput.setAttribute('readonly', '')
+  form.dataset.state = 'error'
 
   setTimeout(() => {
     newTaskInput.placeholder = defaultPlaceholder
     newTaskInput.removeAttribute('readonly')
     newTaskInput.focus()
+    form.dataset.state = ''
   }, 2000)
 }
 
-const addNewTask = (tasks, container, title, taskElements) => {
+const addNewTask = (tasks, container, title, taskElements, data, newTaskInput, form) => {
   const task = { title, done: false }
-  tasks.push(task)
-  const taskMarkup = createTaskMarkup(task)
-  container.insertAdjacentHTML('beforeend', taskMarkup)
-  taskElements.set(container.lastElementChild, task)
+
+  changeData(
+    data,
+    () => {
+      tasks.push(task)
+    },
+    newTaskInput,
+    form
+  ).then(() => {
+    const taskMarkup = createTaskMarkup(task)
+    container.insertAdjacentHTML('beforeend', taskMarkup)
+    taskElements.set(container.lastElementChild, task)
+  })
+}
+
+const changeData = async (data, cb, newTaskInput, form) => {
+  const savedData = copyObjectWithJSON(data)
+  cb(data)
+
+  try {
+    await storeData(data)
+  } catch (e) {
+    replaceAllObjectProperties(data, savedData)
+    showErrorMessage(newTaskInput, ErrorMessage.DATA_SAVE, form)
+    return Promise.reject()
+  }
+}
+
+const storeData = (data) => {
+  return new Promise((resolve, reject) => {
+    const localData = JSON.stringify(data)
+
+    if (localStorage) {
+      localStorage.setItem('todo', localData)
+
+      if (crush) {
+        throw new Error()
+      }
+
+      resolve()
+    } else {
+      reject()
+    }
+  })
 }
 
 initTodo()
